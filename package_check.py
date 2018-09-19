22263d8258f5c97ad805fcf3c0c6de496d2f1837
Ubuntu <juniourkawasakicool@gmail.com>
2018-09-19 19:00:27 +0000

    Added function to check for pip package

--- package_check.py
+++ package_check.py
@@ -20,4 +20,27 @@ def update_system():
         print("System updated")
         logger.info("System Updated")
 
+def pip_check():
+    try:
+        __import__('pip')
+        print("Ok: pip is present")
+        logger.info("Ok: pip is present")
+    except ImportError:
+        print("Installing pip ...")
+        logger.info("Installing pip ..")
+        cmd = ['sudo', 'apt-get', 'install', '-y', 'python3-pip']
+        output, error = execute(cmd)
+        if error:
+            print(error)
+            logger.error(rror)
+        else:
+            print("Ok: pip is installed.")
+            logger.info("Ok: pip is installed")
+    except exception as e:
+        print(e)
+        logger.error(e)
+        sys.exit(1)
+
+pip_check()           
+
 #update_system()
